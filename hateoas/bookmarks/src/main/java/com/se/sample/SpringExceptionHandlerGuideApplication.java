6e1efd269075d2676e8a53f7861baa5c7f9a8d6c
Evgeniy Skiba <eskiba@s-pro.io>
2020-01-09 15:18:07 +0200

    New. Added skeleton for work with hateoas

---
+++ hateoas/bookmarks/src/main/java/com/se/sample/SpringExceptionHandlerGuideApplication.java
@@ -0,0 +1,40 @@
+package com.se.sample;
+
+import com.se.sample.entity.Account;
+import com.se.sample.entity.Bookmark;
+import com.se.sample.repository.AccountRepository;
+import com.se.sample.repository.BookmarkRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class SpringExceptionHandlerGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExceptionHandlerGuideApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(AccountRepository accountRepository,
+                           BookmarkRepository bookmarkRepository) {
+        return (evt) -> Arrays.asList(
+                "jhoeller,dsyer,pwebb,ogierke,rwinch,mfisher,mpollack,jlong".split(","))
+                .forEach(
+                        a -> {
+                            Account account = accountRepository.save(new Account(a,
+                                    "password"));
+                            bookmarkRepository.save(new Bookmark(account,
+                                    "http://bookmark.com/1/" + a, "A description"));
+                            bookmarkRepository.save(new Bookmark(account,
+                                    "http://bookmark.com/2/" + a, "A description"));
+                        });
+    }
+}
