6db00f72b00562ad0c7e694922c2fbb774eadc96
Evgeniy Skiba <eskiba@s-pro.io>
2020-01-09 10:37:32 +0200

    habrexception initial commit
    initial commit for demo spring boot project

---
+++ exceptionHandling/habrexception/src/main/java/com/se/sample/HabrexceptionApplication.java
@@ -0,0 +1,13 @@
+package com.se.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabrexceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabrexceptionApplication.class, args);
+    }
+
+}
