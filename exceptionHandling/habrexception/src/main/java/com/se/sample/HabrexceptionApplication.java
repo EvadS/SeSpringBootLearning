6720364e7bd4df818d25b5b3a938592d7d0671e4
Evgeniy Skiba <eskiba@s-pro.io>
2020-01-09 10:43:21 +0200

    commit title
    commit body

--- exceptionHandling/habrexception/src/main/java/com/se/sample/HabrexceptionApplication.java
+++ exceptionHandling/habrexception/src/main/java/com/se/sample/HabrexceptionApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HabrexceptionApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HabrexceptionApplication.class, args);
     }
