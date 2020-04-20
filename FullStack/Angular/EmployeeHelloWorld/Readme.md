25d5fcc6224cb7f21a76a5e2c452af8e0d32afc9
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2020-04-20 17:32:05 +0300

    Started work with employe service

---
+++ FullStack/Angular/EmployeeHelloWorld/Readme.md
@@ -0,0 +1,32 @@
+
+
+Installing Angular CLI
+## Install node js 
+by downloading the installable from Install NodeJS
+
+## angular cli 
+using the following command.It wll get us the latest version of angular cli.
+
+```bash
+	npm install -g @angular/cli
+```	
+
+
+We can check the angular cli version -
+```bash
+	ng version
+```
+## create project 
+
+```bash
+ng new employee-management
+```
+
+start project 
+```bash
+ng serve
+```
+
+## services
+
+ng generate service service/httpClient
---
+++ FullStack/Angular/EmployeeHelloWorld/SpringEmployeeBackend/src/main/java/com/se/example/demo/model/Employee.java
@@ -0,0 +1,68 @@
+package com.se.example.demo.model;
+
+import java.util.Objects;
+
+public class Employee {
+    private String empId;
+    private String name;
+    private String designation;
+    private double salary;
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Double.compare(employee.salary, salary) == 0 &&
+                empId.equals(employee.empId) &&
+                name.equals(employee.name) &&
+                Objects.equals(designation, employee.designation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(empId, name, designation, salary);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId='" + empId + '\'' +
+                ", name='" + name + '\'' +
+                ", designation='" + designation + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
