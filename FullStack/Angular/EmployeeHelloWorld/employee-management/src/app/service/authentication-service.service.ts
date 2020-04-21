de93be68c31cca3395195a8191338b131f16e000
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2020-04-21 09:12:29 +0300

    sceleton for auth

--- FullStack/Angular/EmployeeHelloWorld/employee-management/src/app/service/authentication-service.service.ts
+++ FullStack/Angular/EmployeeHelloWorld/employee-management/src/app/service/authentication-service.service.ts
@@ -10,7 +10,6 @@ export class User {
 
 }
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -21,34 +20,23 @@ export class AuthenticationService {
   ) {
   }
 
-  // authenticate(username, password) {
-  //   if (username === "javainuse" && password === "password") {
-  //     sessionStorage.setItem('username', username)
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // }
-
   authenticate(username, password) {
-    console.log(username);
     console.log(password);
-    const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
-    return this.httpClient.get<User>('http://localhost:8080/employees/validateLogin', { headers }).pipe(
+    const headers = new HttpHeaders({Authorization: 'Bearer ' + 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqYXZhaW51c2UiLCJleHAiOjE1ODc0NjY3NjYsImlhdCI6MTU4NzQ0ODc2NX0._DIFh3ZSpu2NeYmM5HeQxuwWujCWNtAihCQIDeMrY98VjuDMTR-DSMCRmQSrHXQJZ02TtqU6li51fMLYyWrnsw'
+        // btoa(username + ':' + password)
+      });
+    return this.httpClient.get<User>('http://localhost:8080/employees/validateLogin', {headers}).pipe(
       map(
         userData => {
           sessionStorage.setItem('username', username);
-          const  authString = 'Basic ' + btoa(username + ':' + password);
-          sessionStorage.setItem('basicauth', authString);
           return userData;
         }
       )
-
     );
   }
 
 
-  isUserLoggedIn() {
+  public  isUserLoggedIn() {
     const user = sessionStorage.getItem('username');
     console.log(!(user === null));
     return !(user === null);
--- FullStack/Angular/EmployeeHelloWorld/employee-management/src/app/service/basic-auth-htpp-interceptor-service.service.ts
+++ FullStack/Angular/EmployeeHelloWorld/employee-management/src/app/service/basic-auth-htpp-interceptor-service.service.ts
@@ -9,12 +9,14 @@ export class BasicAuthHttpInterceptorService implements HttpInterceptor {
   constructor() {
   }
 
+
   intercept(req: HttpRequest<any>, next: HttpHandler) {
 
-    if (sessionStorage.getItem('username') && sessionStorage.getItem('basicauth')) {
+    if (sessionStorage.getItem('username')
+      && sessionStorage.getItem('token')) {
       req = req.clone({
         setHeaders: {
-          Authorization: sessionStorage.getItem('basicauth')
+          Authorization: sessionStorage.getItem('token')
         }
       });
     }
