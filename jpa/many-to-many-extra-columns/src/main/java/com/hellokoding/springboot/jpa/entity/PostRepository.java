5e414e28d16943e0692b87e6c7ab64b634079ad1
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2020-07-18 17:38:03 +0300

    Refactored jpa samples

--- jpa/many-to-many-extra-columns/src/main/java/com/hellokoding/springboot/jpa/entity/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hellokoding.springboot.jpa.entity;
-
-import com.hellokoding.springboot.jpa.book.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
