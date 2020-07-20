6ef10a3a764fa8724553bfb27566b148edd04212
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2020-07-20 13:08:05 +0300

    Learning. Spring boot jpa composite key sample

---
+++ jpa/base/cascade-demo/src/main/java/com/se/composite/repository/EmployeeRepository.java
@@ -0,0 +1,19 @@
+package com.se.composite.repository;
+
+
+import com.se.composite.model.Employee;
+import com.se.composite.model.EmployeeIdentity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, EmployeeIdentity> {
+    /*
+       Spring Data JPA will automatically parse this method name
+       and create a query from it
+    */
+    List<Employee> findByEmployeeIdentityCompanyId(String companyId);
+
+}
