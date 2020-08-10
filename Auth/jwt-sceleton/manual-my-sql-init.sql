304aa60cb4051d87147809439d19aa05cdfc134c
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2020-08-10 14:01:11 +0300

    REfactored REadme and init sctipt

--- Auth/jwt-sceleton/manual-my-sql-init.sql
+++ Auth/jwt-sceleton/manual-my-sql-init.sql
@@ -6,19 +6,19 @@ CREATE TABLE `secureDb`.`role_table` (
 
   CREATE TABLE `secureDb`.`user_table` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `login` VARCHAR(45) BINARY NULL,
-  `password` VARCHAR(45) NULL,
-  `role_id` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
+  `login` VARCHAR(45) NULL,
+  `password` VARCHAR(255) NULL,
+  `role_id` INT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `login_UNIQUE` (`login` ASC),
+  UNIQUE INDEX `password_UNIQUE` (`password` ASC),
+  INDEX `fk_user_table_1_idx` (`role_id` ASC),
+  CONSTRAINT `fk_user_table_1`
+    FOREIGN KEY (`role_id`)
+    REFERENCES `secureDb`.`role_table` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
 
-  ALTER TABLE `secureDb`.`user_table`
-ADD INDEX `fk_user_table_1_idx` (`role_id` ASC);
-ALTER TABLE `secureDb`.`user_table`
-ADD CONSTRAINT `fk_user_table_1`
-  FOREIGN KEY (`role_id`)
-  REFERENCES `secureDb`.`role_table` (`name`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
 
   insert into role_table(name) values ('ROLE_ADMIN');
 insert into role_table(name) values ('ROLE_USER');
