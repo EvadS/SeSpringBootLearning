b2da6a6d10d52f9d8bc7a0a5c7e770699ea3cb77
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2020-10-27 07:35:05 +0200

    Spring cloud learning. Eureka
    Demo how to handle registration eureka server registration event

--- SpringCloud/Eureka/EurekaEvents/demo-server/src/main/java/com/se/server/EurekaInstanceCanceledListener.java
+++ SpringCloud/Eureka/EurekaEvents/demo-server/src/main/java/com/se/server/EurekaInstanceCanceledListener.java
@@ -1,8 +1,64 @@
 package com.se.server;
 
+
+import com.netflix.discovery.shared.Applications;
+import com.netflix.eureka.EurekaServerContextHolder;
+import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCanceledEvent;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceRegisteredEvent;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceRenewedEvent;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaRegistryAvailableEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
- * @author Evgeniy Skiba on 27.10.2020
- * @project demo-server
+ * eureka event listener
+ * For example: for monitoring eureka service down notification
+ *
+ * @author hrabbit
+ * @date 2018-09-13
  */
-public class EurekaInstanceCanceledListener {
+@Configuration
+@EnableScheduling
+public class EurekaInstanceCanceledListener implements ApplicationListener {
+    private Logger logger = LoggerFactory.getLogger(EurekaInstanceCanceledListener.class);
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+        // EurekaInstanceCanceledEvent service offline event
+        if (applicationEvent instanceof EurekaInstanceCanceledEvent) {
+            EurekaInstanceCanceledEvent event = (EurekaInstanceCanceledEvent) applicationEvent;
+                         // Get the node information in the current Eureka instance
+            PeerAwareInstanceRegistry registry = EurekaServerContextHolder.getInstance().getServerContext().getRegistry();
+            Applications applications = registry.getApplications();
+            // Traverse to get the node information of the registered node that is consistent with the current invalid node ID.
+            applications.getRegisteredApplications().forEach((registeredApplication) -> {
+                registeredApplication.getInstances().forEach((instance) -> {
+                    if (instance.getInstanceId().equals(event.getServerId())) {
+                        logger.info(" **** Service:" + instance.getAppName() + " Hang up...");
+                        // TODO: 2018-09-13 Extended Message Reminder Email, SMS, WeChat, etc.
+                    }
+                });
+            });
+        }
+        //EurekaInstanceRegisteredEvent service registration event
+        if (applicationEvent instanceof EurekaInstanceRegisteredEvent) {
+            EurekaInstanceRegisteredEvent event = (EurekaInstanceRegisteredEvent) applicationEvent;
+            logger.info(" ******* Service:" + event.getInstanceInfo().getAppName() + " Registration is successful...");
+        }
+        //EurekaInstanceRenewedEvent service renewal event
+        if (applicationEvent instanceof EurekaInstanceRenewedEvent) {
+            EurekaInstanceRenewedEvent event = (EurekaInstanceRenewedEvent) applicationEvent;
+            logger.info("**** Service:" + event.getInstanceInfo().getAppName() + "Renewal...");
+        }
+        //EurekaRegistryAvailableEvent Eureka Registry Startup Event
+        if (applicationEvent instanceof EurekaRegistryAvailableEvent) {
+            logger.info("**** Eureka Registration Center started...");
+        }
+
+    }
 }
--- SpringCloud/Eureka/EurekaEvents/demo-server/src/main/java/com/se/server/EurekaStateChangeListener.java
+++ SpringCloud/Eureka/EurekaEvents/demo-server/src/main/java/com/se/server/EurekaStateChangeListener.java
@@ -1,8 +1,62 @@
 package com.se.server;
 
+import com.netflix.appinfo.InstanceInfo;
+import org.springframework.cloud.netflix.eureka.server.event.*;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
 /**
- * @author Evgeniy Skiba on 27.10.2020
- * @project demo-server
+ * eureka event listener
+ * For example: for monitoring eureka service down notification
+ *
+ * @author hrabbit
+ * @date 2018-09-13
  */
+@Component
 public class EurekaStateChangeListener {
+    /**
+     * EurekaInstanceCanceledEvent service offline event
+     * @param event
+     */
+    @EventListener
+    public void listen(EurekaInstanceCanceledEvent event) {
+        System.out.println(event.getServerId() + "\t" + event.getAppName() + "Service offline");
+    }
+
+    /**
+     * EurekaInstanceRegisteredEvent service registration event
+     * @param event
+     */
+    @EventListener
+    public void listen(EurekaInstanceRegisteredEvent event) {
+        InstanceInfo instanceInfo = event.getInstanceInfo();
+        System.out.println(instanceInfo.getAppName() + "register");
+    }
+
+    /**
+     * EurekaInstanceRenewedEvent service renewal event
+     * @param event
+     */
+    @EventListener
+    public void listen(EurekaInstanceRenewedEvent event) {
+        System.out.println(event.getServerId() + "\t" + event.getAppName() + "Service Renewal");
+    }
+
+    /**
+     * EurekaRegistryAvailableEvent Eureka Registration Center Launch Event
+     * @param event
+     */
+    @EventListener
+    public void listen(EurekaRegistryAvailableEvent event) {
+        System.out.println("Registration Startup");
+    }
+
+    /**
+     * EurekaServerStartedEvent Eureka Server startup event
+     * @param event
+     */
+    @EventListener
+    public void listen(EurekaServerStartedEvent event) {
+        System.out.println("Eureka Server Startup");
+    }
 }
