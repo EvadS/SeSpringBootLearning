76e73eee2fb6a33853275d572b6ebc7c39b04aa3
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2020-10-29 17:55:19 +0200

    Added base CRUD spring jpa project

--- SpringCloud/Eureka/EurekaEvents/demo-client/src/main/java/com/se/client/EurekaClientApplication.java
+++ SpringCloud/Eureka/EurekaEvents/demo-client/src/main/java/com/se/client/EurekaClientApplication.java
@@ -5,11 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
+@EnableFeignClients
 public class EurekaClientApplication implements GreetingController {
 
     @Autowired
