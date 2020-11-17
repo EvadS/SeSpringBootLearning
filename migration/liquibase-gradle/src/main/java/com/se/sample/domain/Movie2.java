01b11d27da75ee92268279c2c8a6c8c5c10c8ae5
Skiba Evgeniy <Skiba.Evgeniy@gmail.com>
2020-11-17 13:42:27 +0200

    Liquibase. Spring boot demo

---
+++ migration/liquibase-gradle/src/main/java/com/se/sample/domain/Movie2.java
@@ -0,0 +1,26 @@
+package com.se.sample.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Movie2 implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String genre;
+
+
+
+}
